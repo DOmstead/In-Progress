5430d071268826bc4dd3bdad378207a0221513a1
David Omstead <49734565+DOmstead@users.noreply.github.com>
2020-04-22 17:57:40 -0700

    Adding Additional Knex Practice Files

---
+++ knex-pratice/blogful-scripts/seed.blogful.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+INSERT INTO blogful_articles
+        (title,date_published,content)
+
+Values
+    ('The Seven Deadly Zins','2016-07-16 12:00:00', 'This is a really good wine'),
+    ('The Baconator', '2018-01-16 12:00:00', 'Super yummy burger'),
+    ('The Mushroom Cheddar Burger', '2019-04-26 12:00:00', 'So much better than the baconator'),
+    ('How to Sew', '2011-08-19 12:00:00', 'A begginers guide to sewing'),
+    ('How to Draw', '2012-03-14 12:00:00', 'A begginers guide to drawing'),
+    ('Yes I am', '2013-07-14 12:00:00', 'How to be true to yourself'),
+    ('Why Not?', '2020-03-09 12:00:00', 'A guide on being direct and getting what you want'),
+    ('I Believe Laura Wasn''t Finished Speaking', '2016-08-18 12:00:00', 'How to stand up for others whose contributions are not recognized'),
+    ('Am I an Ally', '2019-05-17 12:00:00', 'How to help others who need you'),
+    ('Was I In The Wrong?', '2014-03-17 12:00:00', 'Essential self reflections tips to improve yourself'),
+    ('The Eight Deadly Zins','2016-07-16 12:00:00', 'This is a really good wine'),
+    ('The Burberuat', '2018-01-16 12:00:00', 'Super yummy burger'),
+    ('The Ultra Burger', '2019-04-26 12:00:00', 'So much better than the baconator'),
+    ('How to Knit', '2011-08-19 12:00:00', 'A begginers guide to sewing'),
+    ('How to Scream', '2012-03-14 12:00:00', 'A begginers guide to drawing'),
+    ('No, I am Not', '2013-07-14 12:00:00', 'How to be true to yourself'),
+    ('Why?', '2020-03-09 12:00:00', 'A guide on being direct and getting what you want'),
+    ('I Believe Samantha Wasn''t Finished Speaking', '2016-08-18 12:00:00', 'How to stand up for others whose contributions are not recognized'),
+    ('Am I Alive?', '2019-05-17 12:00:00', 'How to help others who need you'),
+    ('Am I Changing?', '2014-03-17 12:00:00', 'Essential self reflections tips to improve yourself');
+
+    COMMIT;
